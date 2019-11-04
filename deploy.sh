9bb4143d236cabc24a20d34227e185a73a3152c8
Simon Alling <alling.simon@gmail.com>
2019-11-04 13:41:08 +0100

    Include .gitconfig in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 readonly CONFIG_FILE=".shellrc" # must not contain spaces
+readonly GITCONFIG=".gitconfig"
 
 # Deploy files
 cp "$CONFIG_FILE" ~
+cp "$GITCONFIG" ~
 
 # Configure Bash
 if [ -f ~/.bashrc ]; then
     echo ". ~/$CONFIG_FILE" >> ~/.bashrc
 fi
-
