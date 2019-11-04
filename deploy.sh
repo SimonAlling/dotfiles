57a20187bdac7ff65d7f15c9e8f63628fc2b98fc
Simon Alling <alling.simon@gmail.com>
2019-11-04 16:26:46 +0100

    Improve shell configuration in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+readonly REAL_RC_FILE="$HOME/.bashrc"
 readonly CONFIG_FILE=".shellrc" # must not contain spaces
+readonly REAL_RC_FILE_ADDITION=". ~/$CONFIG_FILE"
 readonly GITCONFIG=".gitconfig"
 
 # Deploy files
 cp "$CONFIG_FILE" ~
 cp "$GITCONFIG" ~
 
-# Configure Bash
-if [ -f ~/.bashrc ]; then
-    echo ". ~/$CONFIG_FILE" >> ~/.bashrc
+# Configure shell
+if [[ -f "$REAL_RC_FILE" ]]; then
+    if [[ -z $(grep -Fx "$REAL_RC_FILE_ADDITION" "$REAL_RC_FILE") ]]; then
+        echo "Modifying '$REAL_RC_FILE'..."
+        echo "$REAL_RC_FILE_ADDITION" >> "$REAL_RC_FILE"
+    else
+        echo "'$REAL_RC_FILE' exists, but has already been modified."
+    fi
 fi
