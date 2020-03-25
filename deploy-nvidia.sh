775285f6218a99030a3170d827565052f7b1dd7f
Simon Alling <alling.simon@gmail.com>
2020-03-25 16:10:59 +0100

    Add Nvidia fan speed script

---
+++ deploy-nvidia.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+SCRIPTS_DIR='scripts'
+TARGET_DIR='/etc/profile.d'
+SCRIPT_NAME='set-gpu-fan-speed.sh'
+COOL_BITS=4 # https://wiki.archlinux.org/index.php/NVIDIA/Tips_and_tricks#Enabling_overclocking
+
+set -e
+
+echo "Enabling Nvidia fan speed control ..."
+sudo nvidia-xconfig -a --cool-bits=${COOL_BITS} --allow-empty-initial-configuration
+
+echo "Copying fan speed script to '${TARGET_DIR}' ..."
+sudo cp -t "${TARGET_DIR}" "${SCRIPTS_DIR}/${SCRIPT_NAME}"
+sudo chmod 644 "${TARGET_DIR}/${SCRIPT_NAME}"
+
+echo "Done. Fan speed should be automatically set on next login."
---
+++ scripts/set-gpu-fan-speed.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# The only way to set a fan speed below 40 % on my Gigabyte RTX 2060 Super card is to start at 40 % and carefully decrease it from there.
+STEPS="40 30 25"
+# The speed must be given time to stabilize before being changed again; otherwise the fans enter WOOSH WOOSH mode. Stabilization takes about 5 seconds.
+STEP_INTERVAL=7
+
+set -e
+
+for speed in $STEPS; do
+    nvidia-settings -a "[gpu:0]/GPUFanControlState=1" -a "[fan:0]/GPUTargetFanSpeed=$speed"
+    sleep $STEP_INTERVAL
+done
