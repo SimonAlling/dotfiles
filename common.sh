9e0f3d6615492e763e18ae7b394013862798fd9c
Simon Alling <alling.simon@gmail.com>
2020-08-03 10:01:01 +0200

    Add VS Code black screen fix
    I get a black screen when launching VS Code without --disable-gpu with
    an Nvidia GPU.

--- common.sh
+++ common.sh
@@ -1,2 +1,4 @@
+DESKTOP_ENTRIES_SOURCE="desktop-entries"
+DESKTOP_ENTRIES_TARGET="/usr/share/applications"
 SCRIPTS_DIR='scripts'
 PROFILE_SCRIPTS_DIR='/etc/profile.d'
--- deploy-nvidia.sh
+++ deploy-nvidia.sh
@@ -8,6 +8,10 @@ COOL_BITS=31 # https://wiki.archlinux.org/index.php/NVIDIA/Tips_and_tricks#Enabl
 
 set -e
 
+# Deploy VS Code desktop entry with fix for black screen on launch (--disable-gpu):
+echo "Copying VS Code desktop entry to ${DESKTOP_ENTRIES}..."
+sudo cp ${DESKTOP_ENTRIES_SOURCE}/nvidia/code.desktop ${DESKTOP_ENTRIES_TARGET}
+
 echo "Unlocking Nvidia settings ..."
 sudo nvidia-xconfig -a --cool-bits=${COOL_BITS} --allow-empty-initial-configuration
 
