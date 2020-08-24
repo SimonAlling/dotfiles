fe17b40d4c2183294aff2ec91784dee9d0a390ed
Simon Alling <alling.simon@gmail.com>
2020-08-24 16:00:30 +0200

    Remove Nvidia fan speed script
    I don't need it anymore because I've replaced the stock fans.

--- deploy-nvidia.sh
+++ deploy-nvidia.sh
@@ -2,8 +2,6 @@
 
 source common.sh
 
-TARGET_DIR="${PROFILE_SCRIPTS_DIR}"
-SCRIPT_NAME='set-gpu-fan-speed.sh'
 COOL_BITS=31 # https://wiki.archlinux.org/index.php/NVIDIA/Tips_and_tricks#Enabling_overclocking
 
 set -e
@@ -14,9 +12,3 @@ sudo cp ${DESKTOP_ENTRIES_SOURCE}/nvidia/code.desktop ${DESKTOP_ENTRIES_TARGET}
 
 echo "Unlocking Nvidia settings ..."
 sudo nvidia-xconfig -a --cool-bits=${COOL_BITS} --allow-empty-initial-configuration
-
-echo "Copying fan speed script to '${TARGET_DIR}' ..."
-sudo cp -t "${TARGET_DIR}" "${SCRIPTS_DIR}/${SCRIPT_NAME}"
-sudo chmod 644 "${TARGET_DIR}/${SCRIPT_NAME}"
-
-echo "Done. Fan speed should be automatically set on next login."
