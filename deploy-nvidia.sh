e96dda4ec4de269d48bd4550ce7e42f4130bff32
Simon Alling <alling.simon@gmail.com>
2020-08-25 09:22:40 +0200

    Fix bad variable reference in echo message

--- deploy-nvidia.sh
+++ deploy-nvidia.sh
@@ -7,7 +7,7 @@ COOL_BITS=31 # https://wiki.archlinux.org/index.php/NVIDIA/Tips_and_tricks#Enabl
 set -e
 
 # Deploy VS Code desktop entry with fix for black screen on launch (--disable-gpu):
-echo "Copying VS Code desktop entry to ${DESKTOP_ENTRIES}..."
+echo "Copying VS Code desktop entry to ${DESKTOP_ENTRIES_TARGET}..."
 sudo cp ${DESKTOP_ENTRIES_SOURCE}/nvidia/code.desktop ${DESKTOP_ENTRIES_TARGET}
 
 echo "Unlocking Nvidia settings ..."
