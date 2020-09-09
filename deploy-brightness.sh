6ef6cf6ab6e643ffc7c901b1e324d3cd1c7d9908
Simon Alling <alling.simon@gmail.com>
2020-09-09 21:51:57 +0200

    Use ddcutil (not ddccontrol) in brightness script
    ddcutil works with my Asus PG278Q; ddccontrol doesn't.
    
    https://github.com/ddccontrol/ddccontrol/issues/84
    
    ddcutil is also noticeably faster (both with and without --noverify).
    
    Before (ddccontrol):
    
        $ time brightness 0
        real	0m1,003s
        $ time brightness up
        real	0m1,812s
    
    After (ddcutil):
    
        $ time brightness 0
        real	0m0,565s
        $ time brightness up
        real	0m1,123s

--- deploy-brightness.sh
+++ deploy-brightness.sh
@@ -4,9 +4,10 @@ source common.sh
 
 TARGET_DIR='/usr/local/bin'
 SCRIPT_NAME='brightness'
+DDC_TOOL=ddcutil
 
-echo "Making ddccontrol run as root ..."
-sudo chmod +s $(which ddccontrol)
+echo "Making ${DDC_TOOL} run as root ..."
+sudo chmod +s $(which ${DDC_TOOL})
 
 echo "Installing brightness script in '${TARGET_DIR}' ..."
 sudo install "${SCRIPTS_DIR}/${SCRIPT_NAME}" "${TARGET_DIR}"
