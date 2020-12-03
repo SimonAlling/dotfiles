a778e2e82319de13e229a75d8899d73737a07484
Simon Alling <alling.simon@gmail.com>
2020-12-03 17:56:44 +0100

    Fix monitor setup deploy script
    With this change, all monitor setup scripts are copied to /usr/bin.

--- common.sh
+++ common.sh
@@ -4,3 +4,4 @@ DESKTOP_ENTRIES_SOURCE="desktop-entries"
 DESKTOP_ENTRIES_TARGET="/usr/share/applications"
 SCRIPTS_DIR='scripts'
 PROFILE_SCRIPTS_DIR='/etc/profile.d'
+BIN_DIR='/usr/bin'
--- deploy-monitor-setup.sh
+++ deploy-monitor-setup.sh
@@ -9,4 +9,10 @@ SCRIPT_NAME_IN_TARGET_DIR='monitor-setup.sh'
 echo "Copying monitor setup script to '${TARGET_DIR}' ..."
 sudo cp "${SCRIPTS_DIR}/${SCRIPT_NAME}" "${TARGET_DIR}/${SCRIPT_NAME_IN_TARGET_DIR}"
 
+for script in "${SCRIPTS_DIR}"/monitor-setup/*; do
+    executable_name=$(basename "$script" ".sh")
+    echo "Copying $script to '${BIN_DIR}/${executable_name}' ..."
+    sudo cp "$script" "${BIN_DIR}/${executable_name}"
+done
+
 echo "Done. Monitor setup should be automatically applied on next login."
