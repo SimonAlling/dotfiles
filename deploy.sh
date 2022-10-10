ad7ca8002d3e12e79c37f8085d62243dbcdc020d
Simon Alling <alling.simon@gmail.com>
2022-10-10 15:34:22 +0200

    Generalize shell configuration in deploy script
    This makes adding multiple lines straightforward.
    
    💡 `git show --color-words='has already been modified|fi|.'`

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ source common.sh
 
 readonly REAL_RC_FILE="$HOME/.bashrc"
 readonly CONFIG_FILE=".shellrc" # must not contain spaces
-readonly REAL_RC_FILE_ADDITION=". ~/$CONFIG_FILE"
 declare -a FILES_TO_COPY_TO_HOME=(
     "$CONFIG_FILE"
     ".config"
@@ -24,11 +23,15 @@ done
 mkdir -p $HOME/.vimundo
 
 # Configure shell
-if [[ -f "$REAL_RC_FILE" ]]; then
-    if [[ -z $(grep -Fx "$REAL_RC_FILE_ADDITION" "$REAL_RC_FILE") ]]; then
-        echo "Modifying '$REAL_RC_FILE'..."
-        echo "$REAL_RC_FILE_ADDITION" >> "$REAL_RC_FILE"
-    else
-        echo "'$REAL_RC_FILE' exists, but has already been modified."
+function addToRealRcFile() {
+    addition="$1"
+    if [[ -f "$REAL_RC_FILE" ]]; then
+        if [[ -z $(grep -Fx "$addition" "$REAL_RC_FILE") ]]; then
+            echo "Modifying '$REAL_RC_FILE'..."
+            echo "$addition" >> "$REAL_RC_FILE"
+        else
+            echo "'$REAL_RC_FILE' exists, but already contains '$addition'."
+        fi
     fi
-fi
+}
+addToRealRcFile ". ~/$CONFIG_FILE"
